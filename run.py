acd970e61cc1667b77e1683d307c7b6f9035629f
Niles Rogoff <me@niles.xyz>
2017-09-19 12:59:35 -0400

    Works better but rotated 90 degrees still

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 from PIL import Image
 average = lambda x: sum(x)/len(x) if len(x) > 0 else 0
 start = 0x2800
-char_width = 40
-char_height = 10
+char_width = 10
+char_height = 20
 char_width_divided = round(char_width / 2)
 char_height_divided = round(char_height / 4)
-filename = "Pictures/Anthony Foxx official portrait.jpg"
+filename = "../Pictures/Anthony Foxx official portrait.jpg"
 base = Image.open(filename)
 def image_average(x1, y1, x2, y2):
     ret = []
@@ -20,13 +20,13 @@ def convert_index(x):
     if x == 5: return 4
     if x == 6: return 5
     if x == 7: return 7
-for x in range(0, base.width - char_width - 1, char_width):
-    for y in range(0, base.height - char_height - 1, char_height):
+for x in range(0, base.height - char_height - 1, char_height):
+    for y in range(0, base.width - char_width - 1, char_width):
         byte = 0x0
         index = 0
-        for xn in range(2):
-            for yn in range(4):
-                avg = image_average(x + (char_width_divided * xn), y + (char_height_divided * yn), x + (char_width_divided * (xn + 1)), y + (char_height_divided * (yn + 1)))
+        for yn in range(2):
+            for xn in range(4):
+                avg = image_average(x + (char_height_divided * xn), y + (char_width_divided * yn), x + (char_height_divided * (xn + 1)), y + (char_width_divided * (yn + 1)))
                 if avg > 127:
                     byte += 2**convert_index(index)
                 index += 1
