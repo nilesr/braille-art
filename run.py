5e4cab20e0d910356e310bd6957e21f1a799f412
Niles Rogoff <me@niles.xyz>
2017-09-21 01:00:11 -0400

    Add output2.txt

--- run.py
+++ run.py
@@ -1,16 +1,17 @@
 from PIL import Image
-import random
+import random, sys
 average = lambda x: sum(x)/len(x) if len(x) > 0 else 0
 start = 0x2800
-char_width = 10
-char_height = 20
+char_width = 8
+char_height = char_width * 2
 dither = 20
-sensitivity = 0.5
+sensitivity = 0.3
 char_width_divided = round(char_width / 2)
 char_height_divided = round(char_height / 4)
 filename = "../Pictures/Anthony Foxx official portrait.jpg"
-#filename = "../Pictures/smug 55.jpg"
+filename = "../Pictures/anime pepe.png"
 base = Image.open(filename)
+match = lambda a, b: a < b if "--invert" in sys.argv else a > b
 def image_average(x1, y1, x2, y2):
     ret = []
     for x in range(x1, x2):
@@ -31,7 +32,7 @@ for y in range(0, base.height - char_height - 1, char_height):
         for xn in range(2):
             for yn in range(4):
                 avg = image_average(x + (char_height_divided * xn), y + (char_width_divided * yn), x + (char_height_divided * (xn + 1)), y + (char_width_divided * (yn + 1)))
-                if avg + random.randint(-dither, dither) > sensitivity * 0xFF:
+                if match(avg + random.randint(-dither, dither), sensitivity * 0xFF):
                     byte += 2**convert_index(index)
                 index += 1
         print(chr(start + byte), end = "")
