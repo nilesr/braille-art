7ef959921128512178ae86c6527e969281d88d95
Niles Rogoff <me@niles.xyz>
2017-09-21 00:43:36 -0400

    dithering added

--- run.py
+++ run.py
@@ -1,13 +1,15 @@
 from PIL import Image
+import random
 average = lambda x: sum(x)/len(x) if len(x) > 0 else 0
 start = 0x2800
-char_width = 10
-char_height = 10
+char_width = 20
+char_height = 40
+dither = 20
 sensitivity = 0.5
 char_width_divided = round(char_width / 2)
 char_height_divided = round(char_height / 4)
 filename = "../Pictures/Anthony Foxx official portrait.jpg"
-#filename = "../Pictures/2hu.png"
+#filename = "../Pictures/smug 55.jpg"
 base = Image.open(filename)
 def image_average(x1, y1, x2, y2):
     ret = []
@@ -22,17 +24,14 @@ def convert_index(x):
     if x == 5: return 4
     if x == 6: return 5
     if x == 7: return 7
-for y in range(0, base.width - char_width - 1, char_width):
-    for x in range(0, base.height - char_height - 1, char_height):
+for y in range(0, base.height - char_height - 1, char_height):
+    for x in range(0, base.width - char_width - 1, char_width):
         byte = 0x0
         index = 0
         for xn in range(2):
             for yn in range(4):
-                try:
-                    avg = image_average(x + (char_height_divided * xn), y + (char_width_divided * yn), x + (char_height_divided * (xn + 1)), y + (char_width_divided * (yn + 1)))
-                except:
-                    break
-                if avg > sensitivity * 0xFF:
+                avg = image_average(x + (char_height_divided * xn), y + (char_width_divided * yn), x + (char_height_divided * (xn + 1)), y + (char_width_divided * (yn + 1)))
+                if avg + random.randint(-dither, dither) > sensitivity * 0xFF:
                     byte += 2**convert_index(index)
                 index += 1
         print(chr(start + byte), end = "")
